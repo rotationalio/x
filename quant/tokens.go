5b8ae6c150c037279d75a9f156480c41fc48892f
Chris Okuda <chris@okuda.dev>
2025-08-05 19:10:36 -1000

    example changes for a string-based high level API and some todos

--- quant/tokens.go
+++ quant/tokens.go
@@ -21,6 +21,7 @@ Functions:
 
 // Tokenizes a string (naively) by grouping alphanumeric characters, ignoring
 // non-alphanumeric characters. Does not modify the corpus before tokenizing.
+// TODO: replace lang with an options functions thing like patrick did in radish
 func TokenizeStringNaive(corpus string, lang Language) (tokens []string, err error) {
 	var (
 		expr string
@@ -54,6 +55,7 @@ func TokenizeStringNaive(corpus string, lang Language) (tokens []string, err err
 // modifiers provided will be performed before counting. An example of a
 // [StringModifier] would be the function [strings.ToLower] or many others in
 // the Go [strings] package.
+// TODO: replace the modifiers with an options functions thing like patrick did in radish
 func TypeCountStringTokens(tokens []string, tokenModifiers ...StringModifier) (types map[string]int64) {
 	// Make the types map (variable sz was selected arbitrarily)
 	sz := len(tokens) / 4
@@ -71,4 +73,20 @@ func TypeCountStringTokens(tokens []string, tokenModifiers ...StringModifier) (t
 	}
 
 	return types
+}
+
+// ############################################################################
+// HIGH LEVEL API EXAMPLE
+// ############################################################################
+
+type Options func(something any) any //FIXME: make this something useful not just a type filler for now
+
+// Returns a map of type strings and their counts. For each token, all of the
+// modifiers provided will be performed before counting. An example of a
+// [StringModifier] would be the function [strings.ToLower] or many others in
+// the Go [strings] package.
+func TypeCountString(corpus string, options ...Options) (types map[string]int64) {
+	//TODO: type count a string with different tokenizers and other stuff using options functions like patrick did for radish
+	// Make the types map (variable sz was selected arbitrarily)
+	return make(map[string]int64)
 }
