17d87d4f6de39496df5b0cad9d707ca1e401d863
Benjamin Bengfort <benjamin@rotational.io>
2026-05-18 12:03:16 -0500

    Add Clone to DSN Package (#44)

---
+++ dsn/constants.go
@@ -0,0 +1,15 @@
+package dsn
+
+// Common provider and driver names for ease of checking.
+const (
+	Postgres    = "postgres"
+	Psycopg2    = "psycopg2"
+	Psycopg     = "psycopg"
+	CockroachDB = "cockroachdb"
+	MySQL       = "mysql"
+	SQLite3     = "sqlite3"
+	LevelDB     = "leveldb"
+	MongoDB     = "mongodb"
+	Redis       = "redis"
+	Memcached   = "memcached"
+)
