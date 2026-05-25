26ba935535bb403b0fa3d6f7fd2a043e8bf06ac0
Chris Okuda <chris@okuda.dev>
2026-05-25 11:02:17 -1000

    staticcheck!!!!!

--- purser/fuzz/locker_test.go
+++ purser/fuzz/locker_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	"go.rtnl.ai/x/assert"
-	"go.rtnl.ai/x/purser/locker"
 	constv1 "go.rtnl.ai/x/purser/locker/v1/constants"
 )
 
@@ -17,18 +16,11 @@ import (
 // FuzzParseKeyID exercises v1 [locker.Locker.ParseKeyID] on random ciphertext wire.
 // Invariants: no panic; success implies non-empty key id for keyring routing.
 func FuzzParseKeyID(f *testing.F) {
-	var lck locker.Locker
-
-	lck = NewLocker(f, constv1.Edition)
+	lck := NewLocker(f, constv1.Edition)
 	AddEditionWireSeeds(f)
 
 	f.Fuzz(func(t *testing.T, data []byte) {
-		var (
-			kid []byte
-			err error
-		)
-
-		kid, err = lck.ParseKeyID(data)
+		kid, err := lck.ParseKeyID(data)
 		if err != nil {
 			return
 		}
